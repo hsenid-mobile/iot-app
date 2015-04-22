5e012f3db0f8773e04f2dd72705d8f9c9dcc5e17
JayKrish <jayakrishnanr@hsenidmobile.com>
2015-04-22 18:54:20 +0530

    Added sample app to connect with iotkit, done from IoT Webinar

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# iot-app
+This is a sample appliction to talk with IoT device loaded with hSenid Mobiles Iot-kit
+(https://github.com/hsenid-mobile/iot-kit)
+
+This is a nodejs application
+Install ws from node (https://www.npmjs.com/package/ws) and run to connect.
---
+++ iot/my_app.js
@@ -0,0 +1,24 @@
+var WebSocket = require('ws');
+var ws = new WebSocket('ws://devspace.hsenidmobile.com:9008/iot/connect?token=<YOUR_TOKEN>');
+
+ws.on('open', function open() {
+    console.log("connected!")
+});
+
+ws.on('message', function (data, flags) {
+    var message = JSON.parse(data);
+
+    if (message.type == 'boot') {
+        ws.send(
+            JSON.stringify({
+                id: "123456",
+                type: "command",
+                content: "while 1 {if(a0 < 200){print 'level=' a0;}; snooze(1000); };",
+                device: message.device
+            })
+        );
+    }
+
+    console.log(data);
+
+});
